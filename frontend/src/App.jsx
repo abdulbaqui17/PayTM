fb349fda37f676f77f5a2ba4e99f700e662c47b5
abdulbaqui17 <22n31a7343@mrcet.ac.in>
2024-09-09 19:57:59 +0530

    add this path=/

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route exact path="/" element={<Signup/>} />
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
